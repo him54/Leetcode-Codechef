2f6bccca964566819d9600daf3ab02e8c13f38a7
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-09-30 01:38:36 +0530

    Create Best Time to Buy and sell stock with Transection Fee.java

---
+++ Best Time to Buy and sell stock with Transection Fee.java
@@ -0,0 +1,33 @@
+class Solution{
+public int maxProfit(int[] prices, int fee) {
+    int obsp = -prices[0];
+    int ossp = 0;
+
+    for(int i = 0; i<prices.length; i++)
+    {
+      int nbsp = 0;
+      int nssp = 0;
+      
+      if(ossp - prices[i] > obsp)// Bold state profit
+      {
+          nbsp = ossp - prices[i];
+      }
+      else
+      {
+         nbsp = obsp;  
+      }
+      
+      if(obsp + prices[i] - fee > ossp) // Sold state profit
+      {
+          nssp = obsp + prices[i] - fee;
+      }
+      else {
+          nssp = ossp;
+      }
+      obsp = nbsp;
+      ossp = nssp;
+      
+    }
+    return ossp;  
+ }
+}
