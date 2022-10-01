14da672ae163b0d68347da0f000818cc5c3b7cee
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-01 23:55:43 +0530

    Create Bitwise XOR of All pairings.java

---
+++ Bitwise XOR of All pairings.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        
+        int bn1 = nums1.length;
+        int bn2 = nums2.length;
+        
+        int Xor1 = 0;
+        int Xor2 = 0;
+        
+        for(int i = 0; i<bn1; i++)
+        {
+            Xor1 ^= nums1[i];
+            
+        }
+        for(int i = 0; i<bn2; i++)
+        {
+            Xor2 ^= nums2[i];
+        }
+         
+        int bitwiseXor = 0;
+        
+        if(bn1 % 2 == 1 && bn2 % 2 == 0)
+        {
+            bitwiseXor = Xor2;
+        }
+        else if(bn1 % 2 == 0 && bn2 % 2 == 0)
+        {
+            bitwiseXor = 0;
+        }
+        else if(bn1 % 2 == 1 && bn2 % 2 == 1)
+        {
+            bitwiseXor = Xor1 ^ Xor2;
+        }
+         else if(bn1 % 2 == 0 && bn2 % 2 == 1)
+        {
+             bitwiseXor = Xor1;
+        }
+       return bitwiseXor;
+    }
+}
