23bfefe3006bdea70ec386d5b620234bfff92619
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-13 23:07:18 +0530

    Create Detect Capital.java

---
+++ Detect Capital.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() <= 1)
+        {
+            return true;
+        }
+        char[] ch = word.toCharArray();
+        int i = 0;
+        boolean isUpper = false;
+        if(Character.isUpperCase(ch[0]))
+        {
+            i = 1;
+        }
+        if(Character.isUpperCase(ch[i]))
+        {
+            isUpper = true;
+        }
+        for(; i < ch.length; i++){
+            if(isUpper != Character.isUpperCase(ch[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
