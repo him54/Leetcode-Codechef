f8ad23dbc62acc091ed720f810aad04f41577b6f
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-15 00:48:28 +0530

    Create Optimal Partion string.java

---
+++ Optimal Partion string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int partitionString(String s) {
+        int min = 0;
+        HashSet<Character> hs = new HashSet<>();
+        for(char ch : s.toCharArray()){
+            if(hs.contains(ch))
+            {
+                min++;
+                hs = new HashSet<>();
+                hs.add(ch);
+            }
+            else
+            {
+               hs.add(ch); 
+            }
+        }
+            if(!hs.isEmpty())
+            {
+                min++;
+            }
+        return min;
+    }
+}
