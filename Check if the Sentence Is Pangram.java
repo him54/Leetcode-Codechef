9c67a16886f4aa740145c28d9e969de5183ad9dd
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-17 23:52:36 +0530

    Update Check if the Sentence Is Pangram.java

--- Check if the Sentence Is Pangram.java
+++ Check if the Sentence Is Pangram.java
@@ -1,8 +1,7 @@
-//
 class Solution {
     public boolean checkIfPangram(String sentence) {
-       HashSet<Character>hs = new HashSet<>();
-        for(int i = 0; i < sentence.length(); i++) 
+       HashSet<Character>hs = new HashSet<>(); //HashSet contains only unique values
+        for(int i = 0; i < sentence.length(); i++) //We can use a Character HashSet and then check the size of the set if it is 26 then the string is a pangram.
         {
             hs.add(sentence.charAt(i));
         }
