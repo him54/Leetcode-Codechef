c5d3d855818968f118702f1bbbe76330759376bf
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-21 19:13:49 +0530

    Create Chef-dolls.java

---
+++ Chef-dolls.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0 )
+        {
+            int N = sc.nextInt();
+            int ans = 0;
+            for(int i = 0; i<N; i++)
+            {
+                int num = sc.nextInt();
+                ans = ans^num;
+            }
+            System.out.println(ans);
+        }
+    }
+}
