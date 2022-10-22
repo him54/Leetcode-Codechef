6d90d885915cc07666d9289ed1ded5ec85e6041f
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-22 17:06:45 +0530

    Create chef and his apps.java

---
+++ chef and his apps.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0)
+        {
+            int s = sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            
+            int diff = s-(x+y);
+            if(diff>=z)
+            {
+                System.out.println(0);
+            }
+            else{
+                int a = z-diff;
+                int max = Math.max(x,y);
+                if(a > max)
+                {
+                    System.out.println(2);
+                }
+                else{
+                    System.out.println(1);
+                }
+            }
+        }
+    }
+}
