f90232e82efe7074125785c84725d571a336ab81
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-22 18:45:57 +0530

    Create Candy-Destribution.java

---
+++ Candy-Destribution.java
@@ -0,0 +1,30 @@
+//Problem Code:CANDYDIST
+import java.util.*;
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0)
+        {
+          int N =sc.nextInt();
+          int M = sc.nextInt();
+          
+          if(N % M != 0)
+          {
+              System.out.println("NO");
+          }
+          else{
+              int D = N/M;
+              
+              if(D % 2 == 0)
+              {
+                System.out.println("YES");
+              }
+              else{
+                  System.out.println("NO");
+              }
+          }
+        }
+    }
+}
