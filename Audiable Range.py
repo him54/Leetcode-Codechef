17fd093c4f2d945a8b127481822032c6ebe28c01
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-22 18:43:52 +0530

    Update Audiable Range.py

--- Audiable Range.py
+++ Audiable Range.py
@@ -1,8 +1,19 @@
-#Codechef Contest 
-num=int(input())
-for _ in range(num):
-    x=int(input())
-    if 67<=x<=45000:
-        print("YES")
-    else:
-        print("NO")
+import java.util.*;
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0)
+        {
+            int X = sc.nextInt();
+            if(67<=X && X<=45000)
+            {
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
