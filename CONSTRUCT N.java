4d978450cf40ba56844d5d9f01a0c9f1fb5b4fe6
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-23 19:43:12 +0530

    Create CONSTRUCT N.java

---
+++ CONSTRUCT N.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-->0)
+        {
+            int N = sc.nextInt();
+            if(N % 2 == 0 || N % 7 == 0)
+            {
+                System.out.println("YES");
+            }
+            else if(N<7 && N != 2)
+            {
+                System.out.println("NO");
+            }
+            else{
+                int d = N - 9;
+                if(d %2 == 0 || d%7 == 0)
+                {
+                    System.out.println("YES");
+                }
+                else{
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+}
