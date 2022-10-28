b88cce35f33c6a71f74c48575ac6c36c64e0f705
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-10-28 16:18:50 +0530

    Create Group_Anagram.java

---
+++ Group_Anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+       HashMap<HashMap<Character, Integer>, ArrayList<String>> map = new HashMap<>();
+        
+        for(String str : strs)
+        {
+            HashMap<Character, Integer> freqmap = new HashMap<>();
+            for(int i = 0; i<str.length(); i++){
+                char ch = str.charAt(i);
+                freqmap.put(ch, freqmap.getOrDefault(ch, 0) + 1);
+            }
+            
+            if(map.containsKey(freqmap) == false)
+            {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(freqmap, list);
+            }
+            else{
+                ArrayList<String> list = map.get(freqmap);
+                list.add(str);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}
