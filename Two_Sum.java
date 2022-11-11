58f9f8366e067f07c78a0c15c31f2d8c1180b8ba
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-11-11 22:10:20 +0530

    Create Two_Sum.java

---
+++ Two_Sum.java
@@ -0,0 +1,17 @@
+//Time Complexity - O(n);
+import java.util.*;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            if(hm.containsKey(target - nums[i])){
+                arr[1] = i;
+                arr[0] = hm.get(target - nums[i]);
+                return arr;
+            }
+            hm.put(nums[i], i);
+        }
+        return arr;
+    }
+}
