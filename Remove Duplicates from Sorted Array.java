d57219cb0445009960fe2d4d5f8c3d598dd72c09
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-11-11 21:55:31 +0530

    Create Remove Duplicates from Sorted Array.java

---
+++ Remove Duplicates from Sorted Array.java
@@ -0,0 +1,17 @@
+// Time complexity - O(n)
+import java.util.*;
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        int i = 0;
+        for(int j = 0; j<nums.length; j++){
+            if(nums[j] != nums[i]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+}
