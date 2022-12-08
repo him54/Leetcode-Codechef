e77f771bcd08c02e6d0af187c18d1f4980de836f
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-12-08 20:12:53 +0530

    Update Friends_pairing_dp.java

--- Friends_pairing_dp.java
+++ Friends_pairing_dp.java
@@ -11,7 +11,8 @@ public class Main {
         dp[1] = 1;
         dp[2] = 2;
         
-        for(int i = 3; i<=n; i++){
+        for(int i = 3; i<=n; i++)
+        {
             dp[i] = dp[i - 1] + dp[i - 2] * (i - 1);
         }
         System.out.println(dp[n]);
