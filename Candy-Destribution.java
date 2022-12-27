5fb65bebc76ddd8006eed0c6120e450895797f54
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-12-27 22:11:25 +0530

    Update Candy-Destribution.java

--- Candy-Destribution.java
+++ Candy-Destribution.java
@@ -21,7 +21,7 @@ class Main{
               {
                 System.out.println("YES");
               }
-              else
+              else{
                   System.out.println("NO");
               }
           }
