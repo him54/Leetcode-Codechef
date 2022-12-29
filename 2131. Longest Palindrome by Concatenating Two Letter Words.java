f331a49e0a34560233b50e0ea38d5b33333e8352
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2022-12-29 22:27:42 +0530

    Update 2131. Longest Palindrome by Concatenating Two Letter Words.java

--- 2131. Longest Palindrome by Concatenating Two Letter Words.java
+++ 2131. Longest Palindrome by Concatenating Two Letter Words.java
@@ -2,7 +2,7 @@ import java.io.*;
 class Solution {
     public int longestPalindrome(String[] words) {
         int[][] hm = new int[26][26];
-        int max = 0;
+        int max = 0
         for(String word : words){
             hm[word.charAt(0)-'a'][word.charAt(1)-'a']++;
         }
@@ -29,7 +29,7 @@ class Solution {
                     length = length +  4*min;
                 }
                 hm[i][j]=0;
-            
+            }
         }
         if(odd == 1)
         {
