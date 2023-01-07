3cb9bcc8989fd9d8250ee216f0a6391938356f2d
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-01-07 22:02:12 +0530

    Create 6288. Find Consecutive Integers from a Data Stream.java

---
+++ 6288. Find Consecutive Integers from a Data Stream.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+class DataStream {
+    Queue<Integer> que;
+    int n;
+    int total = 0;
+    int val = 0;
+    public DataStream(int value, int k) {
+        que =new LinkedList<>();
+        n = k;
+        val = value;
+    }
+    
+    public boolean consec(int num) {
+        if(que.size() >= n)
+        {
+            int num2 = que.poll();
+            if(num2 == val)
+            {
+                total--;
+            }
+           
+        }
+        if(num == val)
+        {
+            total++;
+        }
+        que.add(num);
+        if(total == n)
+        {
+            return true;
+        }
+        return false;  //Since 2 is less than k, returns False.
+    }
+}
+
+/**
+ * Your DataStream object will be instantiated and called as such:
+ * DataStream obj = new DataStream(value, k);
+ * boolean param_1 = obj.consec(num);
+ */
