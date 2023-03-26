01a4e357d41b6ec2ed4d6c55259965148c762bdf
HIMANSHU MISHRA <73916471+him54@users.noreply.github.com>
2023-03-26 21:41:07 +0530

    Create  K_Items_With_the_Maximum_Sum.java

---
+++ K_Items_With_the_Maximum_Sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        if(k <= numOnes)
+        {
+            return k;
+        }
+        else if(k <= numOnes + numZeros)
+        {
+            return numOnes;
+        }
+        return numOnes + ((k-(numOnes + numZeros))* (-1));
+    }
+}
